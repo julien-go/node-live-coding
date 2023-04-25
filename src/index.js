086782937765000cd454a5feeda5dc9b0c53ef4d
julien-go <juliengodard97@gmail.com>
2023-04-25 16:59:31 +0200

    fix 404

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.post("/api/wilder/addSkill", WilderController.addSkill)
 app.get("/api/skill", SkillController.read)
 app.post("/api/skill", SkillController.create)
 app.put("/api/skill", SkillController.update)
-app.post("/api/skill", SkillController.delete)
+app.delete("/api/skill", SkillController.delete)
 
 app.use((req, res, next) => {
   res.status(404).send("Erreur 404 ! URL not found")
